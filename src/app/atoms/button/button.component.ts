cea9cbfb2b7a846e5b1c641f1fccc0c6fb308fcd
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-03-03 22:01:46 -0500

    feat(Button):Added 'type' and 'preset' properties to ButtonComponent and implemented [attr.type]='type' to enable preset functionality

--- src/app/atoms/button/button.component.ts
+++ src/app/atoms/button/button.component.ts
@@ -20,6 +20,18 @@ export class ButtonComponent {
   @Input() iconPosition: 'left' | 'right' = 'left';
   @Input() fullWidth: boolean = false;
   @Input() disabled: boolean = false;
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() set preset(value: 'submit' | 'reset' | null) {
+    if (value === 'submit') {
+      this.color = 'primary';
+      this.type = 'submit';
+    } else if (value === 'reset') {
+      this.color = 'primary';
+      this.type = 'reset';
+    }
+  }
+  // Propiedad para los presets
+
 
   @Output() click = new EventEmitter<void>();
 // Salida (@Output) para emitir eventos
