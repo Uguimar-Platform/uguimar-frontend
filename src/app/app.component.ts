4ad0ca695e8195b659f244ca7fce1efb80045feb
David Montaño <155041313+XavierM25@users.noreply.github.com>
2025-03-05 11:46:54 -0500

    fix(merge): resolve conflicts in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
-import { faPlus, faUser, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faUser,faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ButtonComponent } from './atoms/button/button.component';
 
 @Component({
   selector: 'app-root',
   imports: [ButtonComponent],
   templateUrl: './app.component.html',
-  /*   styleUrl: './app.component.scss' */
+/*   styleUrl: './app.component.scss' */
 })
 export class AppComponent {
   title = 'frontend';
-  faUser = faUser;
-  plus = faPlus;
+faUser = faUser;
+  plus=faPlus;
   faTrash = faTrash;
-  status: number = 2;
+  status:number=2;
 
-  open() {
-    console.log('open');
+  open(){
+    console.log("open");
   }
 }
