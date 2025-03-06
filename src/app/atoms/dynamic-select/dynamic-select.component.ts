c1bffa23b0ac15f6985b357bb1b6bc2759ac4723
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-03-05 19:21:39 -0500

    create component dynamic-select

---
+++ src/app/atoms/dynamic-select/dynamic-select.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-select',
+  imports: [],
+  templateUrl: './dynamic-select.component.html',
+  styleUrl: './dynamic-select.component.scss'
+})
+export class DynamicSelectComponent {
+
+}
