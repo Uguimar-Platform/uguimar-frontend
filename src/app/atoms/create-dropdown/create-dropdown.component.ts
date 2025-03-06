b730ab19d9095d619dc845d6999bdfafde9e1755
c_lor_c <70907294+JuniorVizquerra@users.noreply.github.com>
2025-03-06 18:09:15 -0500

    Update create-dropdown.component.ts

--- src/app/atoms/create-dropdown/create-dropdown.component.ts
+++ src/app/atoms/create-dropdown/create-dropdown.component.ts
@@ -1,11 +1,16 @@
+/**
+ * This is the CreateDropdownComponent, a basic Angular component
+ * that serves as a foundation for a dropdown feature.
+ */
+
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-create-dropdown',
-  imports: [],
-  templateUrl: './create-dropdown.component.html',
-  styleUrl: './create-dropdown.component.scss'
+  selector: 'app-create-dropdown', // Defines the custom HTML tag for this component
+  imports: [], // Currently, no external modules are imported
+  templateUrl: './create-dropdown.component.html', // Path to the HTML template for the component
+  styleUrl: './create-dropdown.component.scss' // Path to the SCSS file for styling the component
 })
 export class CreateDropdownComponent {
-  
+  // Component logic can be implemented here
 }
