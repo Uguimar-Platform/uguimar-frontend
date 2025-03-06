3e1e2b9c28ea06de465b337d548134e5400e70c5
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-03-06 13:02:57 -0500

    :u

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,67 @@
 import { Component } from '@angular/core';
-import { faPlus, faUser,faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faUser, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ButtonComponent } from './atoms/button/button.component';
 import { DynamicSelectComponent } from './atoms/dynamic-select/dynamic-select.component';
+import { RoutesNavDynamicSelect } from './interfaces/RoutesNavDynamicSelect';
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    ButtonComponent,
-    DynamicSelectComponent
-  ],
-  templateUrl: './app.component.html',
-/*   styleUrl: './app.component.scss' */
+      selector: 'app-root',
+      imports: [
+            ButtonComponent,
+            DynamicSelectComponent
+      ],
+      templateUrl: './app.component.html',
+      /*   styleUrl: './app.component.scss' */
 })
 export class AppComponent {
-  title = 'frontend';
-faUser = faUser;
-  plus=faPlus;
-  faTrash = faTrash;
-  status:number=2;
-
-  open(){
-    console.log("open");
-  }
+      title = 'frontend';
+      faUser = faUser;
+      plus = faPlus;
+      faTrash = faTrash;
+      status: number = 2;
+      public arrayRoutes: RoutesNavDynamicSelect[] = [
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/inicio"
+            },
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/nosotros"
+            },
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/servicios"
+            },
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/contacto"
+            },
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/blog"
+            },
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/galeria"
+            },
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/faq"
+            },
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/privacidad"
+            },
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/terminos"
+            },
+            {
+                  "option": "",
+                  "value": "https://www.ejemplo.com/soporte"
+            }
+      ]
+      open() {
+            console.log("open");
+      }
 }
--- src/app/atoms/dynamic-select/dynamic-select.component.ts
+++ src/app/atoms/dynamic-select/dynamic-select.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, input, InputSignal, OnChanges, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { RoutesNavDynamicSelect } from '../../interfaces/RoutesNavDynamicSelect';
 @Component({
-  selector: 'dynamic-select',
-  imports: [],
-  templateUrl: './dynamic-select.component.html',
-  styleUrl: './dynamic-select.component.scss'
+      selector: 'dynamic-select',
+      imports: [
+            FormsModule,
+            RouterLink
+      ],
+      templateUrl: './dynamic-select.component.html',
+      styleUrl: './dynamic-select.component.scss'
 })
 export class DynamicSelectComponent {
+      protected routeInput = "";
+
+      public arrayRoutes = input.required<RoutesNavDynamicSelect[]>();
+
+      protected selectedRoute: RoutesNavDynamicSelect[] = [];
+
+      protected eventInput() {
+            if (!this.routeInput) {
+                  this.selectedRoute = [];
+                  return;
+            }
+            // Filter the array based on what the user types
+            const filteredRoutes = this.arrayRoutes().filter(route =>
+                  route.value.toLowerCase().includes(this.routeInput.toLowerCase())
+            );
+
+            // update the selectedRoute arrayRoutes
+            this.selectedRoute = filteredRoutes;
+      }
 
 }
