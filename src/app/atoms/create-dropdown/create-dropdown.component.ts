1699a9206d25d4c226871265bd86045246976317
Jennifer CHileno Santisteban <jennifer15aries@gmail.com>
2025-03-06 17:48:36 -0500

    primer commit static

---
+++ src/app/atoms/create-dropdown/create-dropdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-dropdown',
+  imports: [],
+  templateUrl: './create-dropdown.component.html',
+  styleUrl: './create-dropdown.component.scss'
+})
+export class CreateDropdownComponent {
+  
+}
