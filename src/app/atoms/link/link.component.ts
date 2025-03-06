ad9724fb9c01637da0787edb319a572d476c4ebd
Jampier Caramantin <jampiercaramantin06@gmail.com>
2025-03-06 17:48:18 -0500

    Refactor: Change @Input() to readonly

--- src/app/atoms/link/link.component.ts
+++ src/app/atoms/link/link.component.ts
@@ -1,14 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { NgClass } from '@angular/common';  
 /**
  * LinkComponent is a reusable component that renders a navigational link.
- * It allows customization of the displayed label and the target route.
+ * It allows customization of the displayed label, target route, and appearance.
  */
 @Component({
   selector: 'app-link', // Defines the component's selector to be used in templates.
-  imports: [CommonModule, RouterModule], // Imports RouterModule to enable routing functionality.
+  imports: [RouterModule, NgClass], // Imports RouterModule to enable routing functionality.
   templateUrl: './link.component.html', // Specifies the HTML template for the component.
   styleUrl: './link.component.scss' // Defines the stylesheet for component styling.
 })
@@ -17,24 +16,24 @@ export class LinkComponent {
    * The label to be displayed for the link.
    * Default value is 'Enlace'.
    */
-  @Input() label: string = 'Enlace';
+  readonly label = input<string>('Enlace');
 
   /**
    * The route path to navigate when the link is clicked.
    * Default value is an empty string.
    */
-  @Input() route: string = '';
+  readonly route = input<string>('');
 
   /**
    * The color of the link text.
    * Available options: 'primary', 'warning', 'info', 'danger', 'secondary'.
    * Default value is 'primary'.
    */
-  @Input() color: 'primary' | 'warning' | 'info' | 'danger' | 'secondary' = 'primary';
+  readonly color = input<'primary' | 'warning' | 'info' | 'danger' | 'secondary'>('primary');
 
   /**
    * Determines whether the link text should be underlined.
    * Default value is true.
    */
-  @Input()underline: boolean = true;
+  readonly underline = input<boolean>(true);
 }
