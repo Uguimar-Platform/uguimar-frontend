fbb7f35a489609308143486a26bd7b5bcffb1efc
Jwlopezl <jwlopezl@ucvvirtual.edu.pe>
2025-03-08 21:21:02 -0500

    fix: english documentation

--- src/app/atoms/label/label.component.ts
+++ src/app/atoms/label/label.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 /**
- * Componente `LabelComponent`
- * Representa una etiqueta (`label`) personalizable en Angular.
- */
+* `LabelComponent` component
+* Represents a customizable label in Angular.
+*/
 
 /** 
 Method that implements the logic to protect routes.
@@ -13,26 +14,27 @@ It is responsible for verifying the validity of the JWT token and assigning the 
 @returns True if the token is valid, or throws an HTTP exception if it is not.*/
 
 @Component({
-  selector: 'app-label', // Nombre del selector que se usará en el HTML
-  imports: [CommonModule], // Importa CommonModule para utilizar directivas comunes
-  templateUrl: './label.component.html', // Ruta del archivo de plantilla HTML
-  changeDetection: ChangeDetectionStrategy.OnPush, // Estrategia de detección de cambios eficiente
+  selector: 'app-label', // Name of the selector to be used in the HTML
+  imports: [CommonModule], // Import CommonModule to use common directives
+  templateUrl: './label.component.html', // HTML template file path
+  changeDetection: ChangeDetectionStrategy.OnPush, // Efficient change detection strategy
 })
 export class LabelComponent {
-  /** Texto que se mostrará en el label.
-   @param '' (Cadena vacía)
-   */
+
+  /** Text to be displayed in the label.
+  @param '' (Empty string)
+  */
   readonly text = input<string>('');
 
-  /**ID del input al que se asocia el label (para `for` en HTML).
-   * @param '' (Cadena vacía)
-   */
+  /**ID of the input to which the label is associated (for `for` in HTML).
+  * @param '' (Empty string)
+  */
   readonly forId = input<string>('');
 
-  /**  Color del texto del label.
-   * Puede ser uno de los valores predefinidos.
-   * @param 'black'
-   */
+  /** Color of the label text.
+  * Can be one of the predefined values.
+  * @param 'black'
+  */
   readonly color = input<
     | 'black'
     | 'blue-dark'
@@ -45,9 +47,10 @@ export class LabelComponent {
     | 'white'
   >('black');
 
-  /** Familia de la fuente del label.
-   * @param 'font-onest'
-   */
+  
+  /** Font family of the label.
+  * @param 'font-onest'
+  */
   readonly fontFamily = input<
     | 'font-onest' // Onest Font Family
     | 'font-poppins' // Poppins
