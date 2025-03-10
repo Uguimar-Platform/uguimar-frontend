a4cfa40ece25b6f9164e2cc4af9978ef6e2f6f20
Jose Gastelo <joseluis_012004@hotmail.com>
2025-03-10 11:38:48 -0500

    fix: reformat app.component,ts code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faPlus, faUser,faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faUser, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ButtonComponent } from './atoms/button/button.component';
 import { DynamicSelectComponent } from './atoms/dynamic-select/dynamic-select.component';
 import { RoutesNavDynamicSelect } from './interfaces/RoutesNavDynamicSelect';
@@ -8,68 +8,72 @@ import { LabelComponent } from './atoms/label/label.component';
 import { CommonModule } from '@angular/common';
 import { ErrorMessageComponent } from './error-message/error-message.component';
 
-
 @Component({
   selector: 'app-root',
-  imports: [ButtonComponent, LabelComponent, CommonModule,DynamicSelectComponent, LinkComponent,ErrorMessageComponent],
+  imports: [
+    ButtonComponent,
+    LabelComponent,
+    CommonModule,
+    DynamicSelectComponent,
+    LinkComponent,
+    ErrorMessageComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-/*   styleUrl: './app.component.scss' */
+  /*   styleUrl: './app.component.scss' */
 })
 export class AppComponent {
   title = 'frontend';
   faUser = faUser;
-  plus=faPlus;
+  plus = faPlus;
   faTrash = faTrash;
   status: number = 2;
   public arrayRoutes: RoutesNavDynamicSelect[] = [
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/inicio"
-            },
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/nosotros"
-            },
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/servicios"
-            },
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/contacto"
-            },
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/blog"
-            },
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/galeria"
-            },
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/faq"
-            },
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/privacidad"
-            },
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/terminos"
-            },
-            {
-                  "option": "",
-                  "value": "https://www.ejemplo.com/soporte"
-            }
-  ]
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/inicio',
+    },
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/nosotros',
+    },
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/servicios',
+    },
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/contacto',
+    },
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/blog',
+    },
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/galeria',
+    },
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/faq',
+    },
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/privacidad',
+    },
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/terminos',
+    },
+    {
+      option: '',
+      value: 'https://www.ejemplo.com/soporte',
+    },
+  ];
 
   errorMessage: string | null = 'Este es un mensaje de error por default';
 
   open() {
     console.log('open');
   }
-  
 }
-
