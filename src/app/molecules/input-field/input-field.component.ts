5025b57707e7d14c0c9ec69b0fe33bc2f3f2d783
David Montaño <155041313+XavierM25@users.noreply.github.com>
2025-03-11 00:44:13 -0500

    feat: add InputField component

---
+++ src/app/molecules/input-field/input-field.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-field',
+  imports: [],
+  templateUrl: './input-field.component.html',
+  styleUrl: './input-field.component.scss',
+})
+export class InputFieldComponent {}
