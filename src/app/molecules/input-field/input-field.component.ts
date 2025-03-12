8712d9a575b38d6eeb86c7d474bfc69ac3a65af9
David Montaño <155041313+XavierM25@users.noreply.github.com>
2025-03-11 20:25:19 -0500

    feat: create functions of input-field.component.ts & modify input-file.component.html

--- src/app/molecules/input-field/input-field.component.ts
+++ src/app/molecules/input-field/input-field.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from '../../atoms/input/input.component';
+import { ErrorMessageComponent } from '../../atoms/error-message/error-message.component';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-input-field',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputComponent,
+    ErrorMessageComponent,
+  ],
   templateUrl: './input-field.component.html',
-  styleUrl: './input-field.component.scss',
 })
-export class InputFieldComponent {}
+export class InputFieldComponent {
+  readonly label = input<string | undefined>(undefined);
+  readonly type = input<string>('text');
+  readonly placeholder = input<string | undefined>(undefined);
+  readonly disabled = input<boolean>(false);
+  readonly width = input<string>('100%');
+  readonly height = input<string>('40px');
+  readonly customClass = input<string | Record<string, boolean> | undefined>(
+    undefined
+  );
+  readonly labelIcon = input<IconDefinition | undefined>(undefined);
+  readonly errorClass = input<string>('text-red-500 text-sm mt-1');
+
+  control = new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+
+  get hasError(): boolean {
+    return this.control.invalid && (this.control.dirty || this.control.touched);
+  }
+
+  get errorMessage(): string | null {
+    if (this.control.errors?.['required']) {
+      return 'Este campo es obligatorio.';
+    }
+    return null;
+  }
+}
