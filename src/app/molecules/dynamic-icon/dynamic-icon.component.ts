917f8861d91e9c8efaaf9d5ea3e34023d4ab54cd
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-03-13 16:37:47 -0500

    feat: add component dynnami-icon

---
+++ src/app/molecules/dynamic-icon/dynamic-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-icon',
+  imports: [],
+  templateUrl: './dynamic-icon.component.html',
+  styleUrl: './dynamic-icon.component.scss',
+})
+export class DynamicIconComponent {}
