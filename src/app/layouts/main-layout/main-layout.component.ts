cae72480809fc52945894d23fdddd93808d97c02
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-03-17 15:39:05 -0500

    feat: create comppnent

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../shared/organisms/header/header.component';
 import { FooterComponent } from '../../shared/molecules/footer/footer.component';
+import { HeroSectionComponent } from '../../shared/organisms/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, HeaderComponent, FooterComponent],
+  imports: [
+    RouterModule,
+    HeaderComponent,
+    FooterComponent,
+    HeroSectionComponent,
+  ],
   standalone: true,
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
