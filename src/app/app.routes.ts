891b976ba7e6500572a27e96bd17ecea83f98b31
TadeoPG <tadeomanuelportillo@gmail.com>
2025-03-16 23:44:18 -0500

    feat: add main-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./layouts/main-layout/main-layout.component'),
+  },
+  {
     path: 'auth',
     loadComponent: () => import('./layouts/auth-layout/auth-layout.component'),
   },
