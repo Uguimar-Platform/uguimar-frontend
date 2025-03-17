1d872995f9a449b05a52138b3ce388964c45145f
David Montaño <155041313+XavierM25@users.noreply.github.com>
2025-03-17 04:16:57 -0500

    feat(accordion): create standalone accordion component

---
+++ src/app/molecules/accordion/accordion.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  imports: [],
+  templateUrl: './accordion.component.html',
+})
+export class AccordionComponent {}
