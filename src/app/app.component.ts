e63668dae96b5403f5ca7e4ea2f0a02c83dde49d
Eduardo Saldaña <lsaldanatu@ucvvirtual.edu.pe>
2025-03-17 17:46:28 -0500

    feat(ui): Implement Learning Path section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
+import { LearningPathSectionComponent } from './shared/organisms/learning-patch-section/learning-path-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LearningPathSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/shared/organisms/learning-patch-section/learning-path-section.component.ts
@@ -0,0 +1,33 @@
+// learning-path-section.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../../atoms/button/button.component';
+import { LabelComponent } from '../../atoms/label/label.component';
+import { InputComponent } from '../../atoms/input/input.component';
+import { IconCloudComponent } from '../../atoms/icon-cloud/icon-cloud.component';
+import { InputFieldComponent } from '../../molecules/input-field/input-field.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'learning-path-section',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ButtonComponent,
+    LabelComponent,
+    InputComponent,
+    IconCloudComponent,
+    InputFieldComponent,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './learning-path-section.component.html',
+  styleUrl: './learning-path-section.component.scss',
+})
+export class LearningPathSectionComponent {
+  pathInputControl = new FormControl('', { nonNullable: true });
+
+  generatePath() {
+    console.log('Generando ruta personalizada:', this.pathInputControl.value);
+    // Aquí iría la lógica para generar la ruta
+  }
+}
