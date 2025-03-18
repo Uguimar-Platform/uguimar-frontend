fb00675a9b91205f6f88b481a17d98f49f19daac
katteyes <janeth.imatosc@gmail.com>
2025-03-18 01:52:50 -0500

    chore: update style and html structure for card-courses

--- src/app/shared/molecules/card-courses/card-courses.component.ts
+++ src/app/shared/molecules/card-courses/card-courses.component.ts
@@ -5,6 +5,12 @@ import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faChartSimple, faUser } from '@fortawesome/free-solid-svg-icons';
 
+/**
+ * Component that represents a course card.
+ *
+ * This component displays detailed information about a course, including its name,
+ * image, description, instructor, recommended age, price, and category.
+ */
 @Component({
   selector: 'app-cardCourses',
   imports: [CommonModule, LabelComponent, ButtonComponent, FontAwesomeModule],
@@ -12,15 +18,56 @@ import { faChartSimple, faUser } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './card-courses.component.scss',
 })
 export class CardCoursesComponent {
+  /**
+   * Representative image of the course.
+   * @type {string}
+   */
   readonly courseImage = input<string>('ImagenCurso');
+
+  /**
+   * Alternative description for the course image.
+   * @type {string}
+   */
   readonly alt = input<string>('DescripcionCurso');
+
+  /**
+   * Course name.
+   * @type {string}
+   */
   readonly courseName = input<string>('NombreCurso');
-  readonly professor = input<string>('Profesor');
+  /**
+   * Name of the instructor teaching the course.
+   * @type {string}
+   */
+  readonly teacher = input<string>('Profesor');
+
+  /**
+   * Recommended age for taking the course.
+   * @type {string}
+   */
   readonly age = input<string>('Edad');
+
+  /**
+   * Course price.
+   * @type {string}
+   */
   readonly price = input<string>('Precio');
+
+  /**
+   * Category to which the course belongs.
+   * @type {string}
+   */
   readonly category = input<string>('CategoriaCurso');
 
-  // Iconos para profesor y edad
+  /**
+   * User icon imported from FontAwesome.
+   * @type {IconDefinition}
+   */
   protected userIcon = faUser;
+
+  /**
+   * Bar chart icon imported from FontAwesome.
+   * @type {IconDefinition}
+   */
   protected barIcon = faChartSimple;
 }
