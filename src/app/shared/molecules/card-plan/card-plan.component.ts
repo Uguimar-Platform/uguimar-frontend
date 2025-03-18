febdde2a225622325f8371b0539894fedb7046dc
franco13s <fnierihu@ucvvirtual.edu.pe>
2025-03-18 18:57:59 -0500

    Actualizando a readonly

--- src/app/shared/molecules/card-plan/card-plan.component.ts
+++ src/app/shared/molecules/card-plan/card-plan.component.ts
@@ -1,8 +1,8 @@
-import { NgClass, NgFor, NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+import { Component, input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUser, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import { ButtonComponent } from '../../atoms/button/button.component';
+import {  } from '../../atoms/button/button.component';
 
 /**
  * Component representing a subscription plan card.
@@ -10,24 +10,70 @@ import { ButtonComponent } from '../../atoms/button/button.component';
 @Component({
   selector: 'app-card-plan',
   standalone: true,
-  imports: [NgClass, NgFor, NgIf, ButtonComponent, FontAwesomeModule],
+  imports: [NgClass, NgFor, FontAwesomeModule],
   templateUrl: './card-plan.component.html',
   styleUrl: './card-plan.component.scss',
 })
 export class CardPlanComponent {
-  @Input() plan: string = ''; // Nombre del plan
-  @Input() price: number = 0; // Precio del plan
-  @Input() users: number = 1; // Número de usuarios permitidos
-  @Input() benefits: string[] = []; // Lista de beneficios
 
-  // Colores independientes para texto, fondo y botón
-  @Input() textColor: 'blue-dark' | 'blue-medium' | 'blue-light' | 'blue-lightest' | 'blue-lighter' | 'blue-faint' | 'grey-light' | 'white' = 'blue-medium';
-  @Input() bgColor: 'blue-dark' | 'blue-medium' | 'blue-light' | 'blue-lightest' | 'blue-lighter' | 'blue-faint' | 'grey-light' | 'white' = 'white';
-  @Input() btnColor: 'blue-dark' | 'blue-medium' | 'blue-light' | 'blue-lightest' | 'blue-lighter' | 'blue-faint' | 'grey-light' | 'white' = 'blue-dark';
+  /**
+   * Name of the plan.
+   * @param {string} plan - ("Basic", "Dúo", "Expert")
+   */
+  readonly plan = input<string>('');
 
+  /**
+   * Price of the subscription plan.
+   * @param {number} price
+   */
+  readonly price = input<number>(0);
+
+  /**
+   * Number of users allowed.
+   * @param {number} users - ("One", "Two", "Three")
+   */
+  readonly users = input<number>(1);
+
+  /**
+   * List of benefits included in the plan.
+   * @param {string[]} benefits
+   */
+  readonly benefits = input<string[]>([]);
+
+  /**
+   * Color of the text of the card-plan.
+   * @param {string} textColor
+   */
+  readonly textColor = input<'blue-dark' | 'blue-medium' | 'blue-light' | 'blue-lightest' | 'blue-lighter' | 'blue-faint' | 'grey-light' | 'white'>('blue-medium');
+
+  /**
+   * Color of the background of the card-plan.
+   * @param {string} bgColor
+   */
+  readonly bgColor = input<'blue-dark' | 'blue-medium' | 'blue-light' | 'blue-lightest' | 'blue-lighter' | 'blue-faint' | 'grey-light' | 'white'>('white');
+
+  /**
+   * Color of the button of the card-plan.
+   * @param {string} btnColor
+   */
+  readonly btnColor = input<'blue-dark' | 'blue-medium' | 'blue-light' | 'blue-lightest' | 'blue-lighter' | 'blue-faint' | 'grey-light' | 'white'>('blue-dark');
+
+  /**
+   * FontAwesome icon representing a user.
+   * @param
+   */
   faUser = faUser;
+
+  /**
+   * FontAwesome icon representing a checkmark for validation or confirmation.
+   */
   faCheckCircle = faCheckCircle;
 
+
+  /**
+   * Gets the corresponding Tailwind CSS class for the text color.
+   * @returns {string} The Tailwind CSS class for the text color.
+   */
   getTextColorClass(): string {
     const colorMap: Record<string, string> = {
       'blue-dark': 'text-[#081F5C]',
@@ -39,9 +85,13 @@ export class CardPlanComponent {
       'grey-light': 'text-[#E7E7E7]',
       'white': 'text-[#FFFFFF]'
     };
-    return colorMap[this.textColor] || 'text-[#334EAC]';
+    return colorMap[this.textColor()] || 'text-[#334EAC]';
   }
 
+  /**
+   * Gets the corresponding Tailwind CSS class for the background color.
+   * @returns {string} The Tailwind CSS class for the background color.
+   */
   getBackgroundColorClass(): string {
     const colorMap: Record<string, string> = {
       'blue-dark': 'bg-blue-800',
@@ -53,9 +103,13 @@ export class CardPlanComponent {
       'grey-light': 'bg-gray-100',
       'white': 'bg-white'
     };
-    return colorMap[this.bgColor] || 'bg-white';
+    return colorMap[this.bgColor()] || 'bg-white';
   }
 
+  /**
+   * Gets the corresponding Tailwind CSS class for the button background color.
+   * @returns {string} The Tailwind CSS class for the button background color.
+   */
   getButtonBackgroundClass(): string {
     const colorMap: Record<string, string> = {
       'blue-dark': 'bg-blue-800 hover:bg-blue-700',
@@ -67,6 +121,7 @@ export class CardPlanComponent {
       'grey-light': 'bg-gray-100 hover:bg-gray-200',
       'white': 'bg-white hover:bg-gray-100'
     };
-    return colorMap[this.btnColor] || 'bg-blue-800 hover:bg-blue-700';
+    return colorMap[this.btnColor()] || 'bg-blue-800 hover:bg-blue-700';
   }
+
 }
