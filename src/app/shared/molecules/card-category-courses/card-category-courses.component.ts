326be0905db76b3ff0d69855d844d0c19af35769
Jennifer CHileno Santisteban <jennifer15aries@gmail.com>
2025-03-19 22:36:12 -0500

    refactor

--- src/app/shared/molecules/card-category-courses/card-category-courses.component.ts
+++ src/app/shared/molecules/card-category-courses/card-category-courses.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-card-category-courses',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-category-courses.component.html',
   styleUrl: './card-category-courses.component.scss'
 })
 export class CardCategoryCoursesComponent {
-  @Input({required : true}) title!: string;
-  @Input({required : true}) course!: string;
-  @Input({required : true}) description!: string;
-  @Input({required : true}) image!: string;
-  @Input({required : true}) buttonText!: string;
-  @Input({required : true}) buttonLink!: string;
+  readonly title = input<string>('')
+  readonly course = input<string>('')
+  readonly description = input<string>('')
+  readonly image = input<string>('')
+  readonly buttonText = input<string>('')
+  readonly buttonLink = input<string>('')
 }
