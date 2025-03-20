7ef0ba6e5f94d9e02da183df7674fd8462be860c
Marisola20 <emedinada@ucvvirtual.edu.pe>
2025-03-19 23:08:09 -0500

    feat: update archives of card-category-courses

--- src/app/shared/molecules/card-category-courses/card-category-courses.component.ts
+++ src/app/shared/molecules/card-category-courses/card-category-courses.component.ts
@@ -1,17 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 
+/**
+ * Component that represents a course category card.
+ */
 @Component({
   selector: 'app-card-category-courses',
   imports: [CommonModule],
   templateUrl: './card-category-courses.component.html',
-  styleUrl: './card-category-courses.component.scss'
+  styleUrl: './card-category-courses.component.scss',
 })
 export class CardCategoryCoursesComponent {
-  readonly title = input<string>('')
-  readonly course = input<string>('')
-  readonly description = input<string>('')
-  readonly image = input<string>('')
-  readonly buttonText = input<string>('')
-  readonly buttonLink = input<string>('')
+  /**
+   * Title of the card.
+   * @param '' (Empty string)
+   */
+  readonly title = input<string>('');
+
+  /**
+   * Name of the course associated with the card.
+   * @param '' (Empty string)
+   */
+  readonly course = input<string>('');
+
+  /**
+   * Brief description of the course.
+   * @param '' (Empty string)
+   */
+  readonly description = input<string>('');
+
+  /**
+   * URL of the image to be displayed on the card.
+   * @param '' (Empty string)
+   */
+  readonly image = input<string>('');
+
+  /**
+   * Text to be displayed on the action button.
+   * @param '' (Empty string)
+   */
+  readonly buttonText = input<string>('');
+
+  /**
+   * Link to which the action button will redirect.
+   * @param '' (Empty string)
+   */
+  readonly buttonLink = input<string>('');
 }
