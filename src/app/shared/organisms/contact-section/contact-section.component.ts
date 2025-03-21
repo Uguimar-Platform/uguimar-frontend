3f44cd8d19dac12e4dfbdfc9817ac26585e314ff
Jose Gastelo <joseluis_012004@hotmail.com>
2025-03-21 12:37:06 -0500

    feat(contact-section): added LabelComponent and documented the ContactSectionComponent class

--- src/app/shared/organisms/contact-section/contact-section.component.ts
+++ src/app/shared/organisms/contact-section/contact-section.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../atoms/button/button.component';
 import { InputFieldComponent } from '../../molecules/input-field/input-field.component';
+import { LabelComponent } from '../../atoms/label/label.component';
+
+/**
+ * @Component ContactSectionComponent
+ * This component represents the contact section of a webpage.
+ * It consists of several input elements, labels, and a button that allow users to send messages or communicate via a form.
+ */
+
 @Component({
   selector: 'app-contact-section',
-  imports: [ButtonComponent, InputFieldComponent],
+  imports: [ButtonComponent, InputFieldComponent, LabelComponent],
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.scss',
 })
