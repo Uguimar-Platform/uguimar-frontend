e44fe82b248f31daece646b7c43b27f9aa56bece
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-03-21 00:59:02 -0500

    refactor: use attribute icon of button-atom in hero-secto and use component label-atom and rename images y format to webp

--- src/app/shared/organisms/hero-section/hero-section.component.ts
+++ src/app/shared/organisms/hero-section/hero-section.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { ButtonComponent } from '../../atoms/button/button.component';
 import { DynamicSelectComponent } from '../../atoms/dynamic-select/dynamic-select.component';
+import { LabelComponent } from '../../atoms/label/label.component';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [ButtonComponent, DynamicSelectComponent],
+  imports: [ButtonComponent, DynamicSelectComponent, LabelComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
-export class HeroSectionComponent {}
+export class HeroSectionComponent {
+  /* 
+    This images property is used for hero section images.
+   */
+  protected images = input<string[]>([
+    '/assets/images/1.webp',
+    '/assets/images/2.webp',
+    '/assets/images/3.webp',
+  ]);
+}
