5b13d89a1d7a90ebbdf1abef71661314e2a74c8e
Eduardo Saldaña <lsaldanatu@ucvvirtual.edu.pe>
2025-03-21 19:03:00 -0500

    fix: fix learning path setion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
-import { LearningPathSectionComponent } from './shared/organisms/learning-patch-section/learning-path-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LearningPathSectionComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
