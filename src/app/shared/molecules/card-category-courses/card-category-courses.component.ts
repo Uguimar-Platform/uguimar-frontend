b3f75c7de123e9096e7fc2269c69d85ba45a09a9
TadeoPG <tadeomanuelportillo@gmail.com>
2025-03-24 00:47:13 -0500

    fix: fix design card-category-courses

--- src/app/shared/molecules/card-category-courses/card-category-courses.component.ts
+++ src/app/shared/molecules/card-category-courses/card-category-courses.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { ButtonComponent } from '../../atoms/button/button.component';
 import { LabelComponent } from '../../atoms/label/label.component';
+import { RouterModule } from '@angular/router';
 
 /**
  * Component that represents a course category card.
  */
 @Component({
   selector: 'app-card-category-courses',
-  imports: [CommonModule, ButtonComponent, LabelComponent],
+  imports: [CommonModule, ButtonComponent, LabelComponent, RouterModule],
   templateUrl: './card-category-courses.component.html',
   styleUrl: './card-category-courses.component.scss',
 })
